322b6e5bda53ddb6ac49371bcca583dd64a97660
AlexSvet117 <107494487+AlexSvet117@users.noreply.github.com>
2024-12-15 23:47:21 +0000

    JS implementation draft

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
-// Add your code here
+// Array of card's numbers and letters
+const lettersNumbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
+
+// Array of emojis
+const emojis = ["♥", "♦", "♠", "♣"]
+
+// Function that shuffles the cards
+function shuffleCards() {
+
+}
